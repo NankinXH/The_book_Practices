20b0b2fd4f490b590a5c5b7d676964caf15be3a5
NankinXH <finn108039@gmail.com>
2023-09-08 18:25:11 +0800

    add some practices

--- 12.1_command_line_program/minigrep/src/lib.rs
+++ 12.1_command_line_program/minigrep/src/lib.rs
@@ -1,29 +1,94 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
-  pub query: String,
-  pub file_path: String,
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
-
-
 
 impl Config {
-  pub fn build(args: &[String]) -> Result<Config, &'static str> {
-      if args.len() < 3 {
-          return Err("not enough arguments");
-      }
-      let query = args[1].clone();
-      let file_path: String = args[2].clone();
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path: String = args[2].clone();
 
-      Ok(Config { query, file_path })
-  }
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
+    }
 }
+// We can search the result in the content we hardcode into the code before in different mode.
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-  println!("Searching for {}", config.query);
-  println!("In file {}", config.file_path);
+    let content: String = fs::read_to_string(config.file_path)?;
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &content)
+    } else {
+        search_case_sensitive(&config.query, &content)
+    };
 
-  let content: String = fs::read_to_string(config.file_path)?;
-  println!("With text: \n{content}");
-  Ok(())
+    for line in results {
+        println!("{line}");
+    }
+    Ok(())
+}
+
+pub fn search_case_sensitive<'a>(query: &'a str, content: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&'a str> = Vec::new();
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &'a str, content: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&'a str> = Vec::new();
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use std::vec;
+
+    use super::*;
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search_case_sensitive(query, content)
+        );
+    }
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.
+";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, content)
+        );
+    }
 }
--- 12.1_command_line_program/minigrep/src/main.rs
+++ 12.1_command_line_program/minigrep/src/main.rs
@@ -1,21 +1,16 @@
-use std::env;
 use minigrep::Config;
+use std::env;
 use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let config: Config = parse_config(&args);
-    if let Err(e) =  minigrep::run(config){
+    // You could use the Error printer in the unwrap_or_else scope.
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1)
     }
 }
-
-pub fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path: String = args[2].clone();
-    Config { query, file_path }
-  }
-
-
