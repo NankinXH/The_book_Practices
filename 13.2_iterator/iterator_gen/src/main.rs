f77e646bbfafc9d152c286b6b16ff4c9490bb7bd
NankinXH <finn108039@gmail.com>
2023-09-11 18:22:03 +0800

    add a line to test github services

--- 13.2_iterator/iterator_gen/src/main.rs
+++ 13.2_iterator/iterator_gen/src/main.rs
@@ -2,4 +2,7 @@ fn main() {
     let the_iterator = vec![1, 22, 44];
     let new_iterator = the_iterator.iter().map(|x|x+1);
     println!("{:#?}", new_iterator);
+    let new_new_vec:Vec<_> = new_iterator.map(|x|x+1).collect();
+    println!("{:?}",new_new_vec);
 }
+
