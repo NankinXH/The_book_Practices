f2589918728ca05738c7aca31769c2fd346e3a1c
NankinXH <finn108039@gmail.com>
2023-09-21 14:32:26 +0800

    add a line to test github services

--- 12.1_command_line_program/minigrep/src/lib.rs
+++ 12.1_command_line_program/minigrep/src/lib.rs
@@ -9,12 +9,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path: String = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg)=>arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let file_path =match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path string"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
@@ -25,6 +30,7 @@ impl Config {
         })
     }
 }
+
 // We can search the result in the content we hardcode into the code before in different mode.
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content: String = fs::read_to_string(config.file_path)?;
@@ -41,23 +47,11 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search_case_sensitive<'a>(query: &'a str, content: &'a str) -> Vec<&'a str> {
-    let mut results: Vec<&'a str> = Vec::new();
-    for line in content.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    content.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &'a str, content: &'a str) -> Vec<&'a str> {
-    let mut results: Vec<&'a str> = Vec::new();
-    for line in content.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    content.lines().filter(|line|line.to_lowercase().contains(query)).collect()
 }
 
 #[cfg(test)]
